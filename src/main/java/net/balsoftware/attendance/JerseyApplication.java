089315ad1d2a108675085851d3fabaacf61bc4d3
David <david@kidreports.com>
2019-06-08 22:26:21 -0600

    changed resource config path to resources

---
+++ src/main/java/net/balsoftware/attendance/JerseyApplication.java
@@ -0,0 +1,33 @@
+package net.balsoftware.attendance;
+
+//import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@Slf4j
+public class JerseyApplication extends ResourceConfig {
+
+    public JerseyApplication() {
+        log.info("setting up hk2");
+        packages("net.balsoftware.attendance");
+
+//        JacksonJaxbJsonProvider jacksonJaxbJsonProvider = new JacksonJaxbJsonProvider();
+//        jacksonJaxbJsonProvider.setMapper(new ObjectMapperFactory().buildObjectMapper());
+//        register(jacksonJaxbJsonProvider);
+        // TODO - USE GSON
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+
+                // Shop to manually bind objects, in the case that the Jersey Auto-scan isn't working
+                // e.g. bind(x.class).to(y.class).in(Singleton.class);
+                // e.g. bind(x.class).to(y.class);
+                //
+                // note: if the object is generic, use TypeLiteral
+                // e.g. bind(x.class).to(new TypeLiteral&lt;InjectionResolver&gt;(){});
+                //
+            }
+        });
+    }
+}
--- src/main/java/net/balsoftware/attendance/Main.java
+++ src/main/java/net/balsoftware/attendance/Main.java
@@ -2,6 +2,8 @@ package net.balsoftware.attendance;
 
 import lombok.extern.slf4j.Slf4j;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -24,11 +26,13 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.dekses.jersey.docker.demo package
-        final ResourceConfig rc = new ResourceConfig().packages("net.balsoftware.attendance");
+        ServiceLocator locator = ServiceLocatorUtilities.createAndPopulateServiceLocator();
+        final ResourceConfig rc = new ResourceConfig().packages("net.balsoftware.attendance.student");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), new JerseyApplication(), locator);
+//        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
     /**
